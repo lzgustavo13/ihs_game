9bc28f1b985f4addd0539136675bf3185a3161f7
lzgustavo13 <148971633+lzgustavo13@users.noreply.github.com>
2024-07-24 14:30:24 -0300

    Update player.py

--- ihs_game/Space-invaders/code/player.py
+++ ihs_game/Space-invaders/code/player.py
@@ -1,28 +1,35 @@
-import pygame
+import pygame 
 from laser import Laser
+from fcntl import ioctl
+from integracao import *
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, pos, screen_width, speed):
+    def __init__(self,pos,constraint,speed):
         super().__init__()
         self.image = pygame.image.load('../graphics/player.png').convert_alpha()
-        self.rect = self.image.get_rect(midbottom=pos)
+        self.rect = self.image.get_rect(midbottom = pos)
         self.speed = speed
-        self.screen_width = screen_width
+        self.max_x_constraint = constraint
         self.ready = True
         self.laser_time = 0
         self.laser_cooldown = 600
+
         self.lasers = pygame.sprite.Group()
 
+        self.laser_sound = pygame.mixer.Sound('../audio/laser.wav')
+        self.laser_sound.set_volume(0.5)
+        
     def get_input(self):
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_RIGHT]:
+        Integration=IO()
+        if Integration.get_PB(0)==0:
             self.move_right()
-        elif keys[pygame.K_LEFT]:
+        if Integration.get_PB(1)==0:
             self.move_left()
-        if keys[pygame.K_SPACE] and self.ready:
+        if Integration.get_PB(3)==0 and self.ready:
             self.shoot()
             self.ready = False
             self.laser_time = pygame.time.get_ticks()
+            self.laser_sound.play()
 
     def recharge(self):
         if not self.ready:
@@ -33,17 +40,15 @@ class Player(pygame.sprite.Sprite):
     def constraint(self):
         if self.rect.left <= 0:
             self.rect.left = 0
-        if self.rect.right >= self.screen_width:
-            self.rect.right = self.screen_width
-
+        if self.rect.right >= self.max_x_constraint:
+            self.rect.right = self.max_x_constraint
     def move_left(self):
-        self.rect.x += self.speed
-        self.constraint()
+            self.rect.x -= self.speed
+            self.constraint()
 
     def move_right(self):
-        self.rect.x -= self.speed
+        self.rect.x += self.speed
         self.constraint()
-
     def shoot(self):
         if self.ready:
             laser = Laser(self.rect.center, -8, self.rect.bottom)
