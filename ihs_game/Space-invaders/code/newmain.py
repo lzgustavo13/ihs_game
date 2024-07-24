ccdac8fa3154f3658e104e5d8a95093bada65060
lzgustavo13 <148971633+lzgustavo13@users.noreply.github.com>
2024-07-24 13:32:52 -0300

    Update newmain.py

--- ihs_game/Space-invaders/code/newmain.py
+++ ihs_game/Space-invaders/code/newmain.py
@@ -1,118 +1,12 @@
 import os, sys
 from fcntl import ioctl
+from integracao import *
 from player import Player
 import obstacle
 from alien import Alien, Extra
 from random import choice, randint
 from laser import Laser
 import pygame
-
-# Definições de valores dos displays de 7 segmentos
-HEX_0 = 0xC0
-HEX_1 = 0xF9
-HEX_2 = 0xA4
-HEX_3 = 0xB0
-HEX_4 = 0x99
-HEX_5 = 0x92
-HEX_6 = 0x82
-HEX_7 = 0xF8
-HEX_8 = 0x80
-HEX_9 = 0x90
-HEX_A = 0x88
-HEX_B = 0x83
-HEX_C = 0xC6
-HEX_D = 0xA1
-HEX_E = 0x86
-HEX_F = 0x8E
-
-PB = 24930
-SW = 24929
-DIS_L = 24931
-DIS_R = 24932
-LED_R = 24933
-LED_G = 24934
-
-class IO:
-
-    def __init__(self) -> None:
-        self.fd = os.open('/dev/mydev', os.O_RDWR)
-        self.dev = SW
-
-    def __del__(self):
-        os.close(self.fd)
-
-    def get_SW(self, pos):
-        ioctl(self.fd, SW)
-        sw_pos = (0x1 << pos)
-        os.read(self.fd, 4)
-        ret_sw = os.read(self.fd, 4)
-        return 1 if (sw_pos & int.from_bytes(ret_sw, 'little')) > 0 else 0
-    
-    def get_PB(self, pos):
-        ioctl(self.fd, PB)
-        pb_pos = (1 << pos)
-        os.read(self.fd, 4)
-        ret_pb = os.read(self.fd, 4)
-        data_ret = 1 if (pb_pos & int.from_bytes(ret_pb, 'little')) > 0 else 0
-        return data_ret
-
-    def put_LD(self, val):
-        ioctl(self.fd, LED_R)
-        os.write(self.fd, val.to_bytes(4, 'little'))
-
-    def put_ar_LD(self, list_pos):
-        ioctl(self.fd, LED_R)
-        data = 0
-        for num in list_pos:
-            data = (1 << num) | data
-        os.write(self.fd, data.to_bytes(4, 'little'))
-
-    def put_DP(self, pos, ar_num):
-        if pos == 0:
-            ioctl(self.fd, DIS_R)
-        else:
-            ioctl(self.fd, DIS_L)
-
-        data = 0
-        for num in ar_num:
-            data = self.__aux_DP(data, num, 8)
-        os.write(self.fd, data.to_bytes(4, 'little'))
-
-    def __aux_DP(self, data, num, ind):
-        data = data << ind
-        if num == '0':
-            data = data | HEX_0
-        elif num == '1':
-            data = data | HEX_1
-        elif num == '2':
-            data = data | HEX_2
-        elif num == '3':
-            data = data | HEX_3
-        elif num == '4':
-            data = data | HEX_4
-        elif num == '5':
-            data = data | HEX_5
-        elif num == '6':
-            data = data | HEX_6
-        elif num == '7':
-            data = data | HEX_7
-        elif num == '8':
-            data = data | HEX_8
-        elif num == '9':
-            data = data | HEX_9
-        elif num == 'A':
-            data = data | HEX_A
-        elif num == 'B':
-            data = data | HEX_B
-        elif num == 'C':
-            data = data | HEX_C
-        elif num == 'D':
-            data = data | HEX_D
-        elif num == 'E':
-            data = data | HEX_E
-        elif num == 'F':
-            data = data | HEX_F
-        return data
 
 class Game:
     def __init__(self):
@@ -281,7 +175,14 @@ class Game:
 
         self.io.put_DP(0, score_str)  # Atualiza o display direito com a pontuação
         self.io.put_DP(1, lives_str)  # Atualiza o display esquerdo com as vidas
-
+    def update_led_score(self):
+        # Calcular o número de LEDs a serem acesos com base na pontuação
+        num_leds_to_light = self.score // 400
+        array = []
+        # Atualizar o estado dos LEDs
+        for i in range(num_leds_to_light):
+            array.append(i)
+            self.io.put_ar_LD(array)  # Substitua por sua função específica para acender LEDs
     def run(self):
         self.player.update()
         self.aliens.update(self.alien_direction)
@@ -300,6 +201,8 @@ class Game:
         self.display_score()
         self.victory_message()
         self.update_display()
+        self.update_led_score()
+        
 
 if __name__ == '__main__':
     pygame.init()
@@ -327,6 +230,9 @@ if __name__ == '__main__':
             game.player.sprite.move_right()
         if not game.io.get_PB(3): 
             game.player.sprite.shoot()
+        if game.io.get_SW(0): 
+            game.__init__()
+
 
         screen.fill((30, 30, 30))
         game.player.sprite.recharge()
